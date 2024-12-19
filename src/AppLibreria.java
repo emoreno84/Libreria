0c1c70dd9276f86225ddfa1c5254d6e68af1beff
eduardo <edumoreno84@gmail.com>
2024-12-19 13:03:37 +0100

    AppLibreria -> Iniciar() (sin terminar)             -> Anyadir() (sin terminar)             -> menu()

--- src/AppLibreria.java
+++ src/AppLibreria.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class AppLibreria {
 
-    Scanner teclado = new Scanner(System.in);
+    static Scanner teclado = new Scanner(System.in);
     int opcion;
     public void iniciar(Libreria libreria){
         do {
@@ -20,6 +20,32 @@ public class AppLibreria {
 
         } while (opcion != 5);
     }
+
+    private static void anyadir(){
+        System.out.println("Indique el ISBN: ");
+        String isbn = teclado.nextLine();
+        System.out.println("Indique el Título: ");
+        String titulo = teclado.nextLine();
+        System.out.println("Indique el Autor: ");
+        String autor = teclado.nextLine();
+        System.out.println("Indique el número de páginas: ");
+        int numPaginas = teclado.nextInt();
+        System.out.println("Elija el género: ");
+        System.out.println("\t1 - Fantasía");
+        System.out.println("\t2 - Ciencia Ficción");
+        System.out.println("\t3 - Novela");
+        System.out.println("\t4 - Biografía");
+        System.out.println("\t5 - Histórico");
+        int opcionGenero = teclado.nextInt();
+
+        System.out.println("");
+
+    }
+
+    private Enums.Genero mapearNumeroGenero(){
+        return null;
+    }
+
     public int menu(){
         System.out.println("Elija una opción...");
         System.out.println("1 - Añadir libro");
